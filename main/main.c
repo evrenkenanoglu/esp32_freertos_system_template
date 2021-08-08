b9527a93fe9653f37cd28700fc02c9a9000c77af
Kenanoglu Evren (IFAT DCGR ATV PS PD CDF) <evren.kenanoglu@infineon.com>
2021-08-08 17:19:14 +0200

    Add: Porting files and main application.

--- main/main.c
+++ main/main.c
@@ -1,14 +1,42 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-08-07
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 
+#include "demoApp.h"
+#include "system.h"
+
+APP_DEMO_CREATE(appDemo, eProcessDemo)
+
+static void startupDelay(void);
 
 void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    startupDelay();
+    while (1)
+    {
+        processStart(&appDemo);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        processSuspend(&appDemo);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        processResume(&appDemo);
+        vTaskDelay(3000/ portTICK_PERIOD_MS);
+        processTerminate(&appDemo);
+        vTaskDelay(3000/ portTICK_PERIOD_MS);
+    }
+}
+
+void startupDelay()
+{
+    for (int i = 3; i >= 0; i--)
+    {
+        printf("Starting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
